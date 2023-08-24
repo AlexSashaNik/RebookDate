099e444c6fcf0a2bb42a69557c7ea8b16cf2be9e
AlexNikitina <alex.nikitina82@gmail.com>
2023-08-24 15:33:57 +0200

    test changed

--- src/test/java/ru/netology/date/DataGenerator.java
+++ src/test/java/ru/netology/date/DataGenerator.java
@@ -6,6 +6,8 @@ import lombok.Value;
 import java.util.Locale;
 import java.time.format.DateTimeFormatter;
 import java.time.LocalDate;
+import java.util.Random;
+
 public class DataGenerator {
     private DataGenerator() {
     }
@@ -13,43 +15,31 @@ public class DataGenerator {
     public static Faker faker = new Faker(new Locale("ru"));
 
     public static String generateDate(int shift) {
-        // TODO: добавить логику для объявления переменной date и задания её значения, для генерации строки с датой
-        // Вы можете использовать класс LocalDate и его методы для получения и форматирования даты
         DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         LocalDate date = LocalDate.now();
         return date.plusDays(shift).format(dateFormat);
     }
 
-    public static String generateCity(String locale) {
-        // TODO: добавить логику для объявления переменной city и задания её значения, генерацию можно выполнить
-        // с помощью Faker, либо используя массив валидных городов и класс Random
-        String city = faker.address().city();
-        return city;
+    public static String generateCity() {
+        var cities = new String[]{"Москва", "Казань", "Калининград", "Санкт-Петербург", "Екатеринбург", "Владивосток"};
+        return cities [new Random().nextInt(cities.length)];
     }
 
     public static String generateName(String locale) {
-        // TODO: добавить логику для объявления переменной name и задания её значения, для генерации можно
-        // использовать Faker
-        String name = faker.name().lastName() + " " + faker.name().firstName();
-        return name;
-    }
+        var faker = new Faker(new Locale(locale));
+        return faker.name().lastName() + " " + faker.name().firstName();
 
+    }
     public static String generatePhone(String locale) {
-        // TODO: добавить логику для объявления переменной phone и задания её значения, для генерации можно
-        // использовать Faker
-        String phone = faker.phoneNumber().phoneNumber();
-        return phone;
+        var faker = new Faker(new Locale(locale));
+        return faker.phoneNumber().phoneNumber();
     }
-
     public static class Registration {
         private Registration() {
         }
 
         public static UserInfo generateUser(String locale) {
-            // TODO: добавить логику для создания пользователя user с использованием методов generateCity(locale),
-            // generateName(locale), generatePhone(locale)
-            UserInfo user = new UserInfo(generateCity(locale), generateName(locale), generatePhone(locale));
-            return user;
+            return new UserInfo(generateCity(), generateName(locale), generatePhone(locale));
         }
 
         @Value
--- src/test/java/ru/netology/date/RebookDateTest.java
+++ src/test/java/ru/netology/date/RebookDateTest.java
@@ -27,11 +27,6 @@ class RebookDateTest {
         var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
         var daysToAddForSecondMeeting = 7;
         var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
-        // TODO: добавить логику теста в рамках которого будет выполнено планирование и перепланирование встречи.
-        // Для заполнения полей формы можно использовать пользователя validUser и строки с датами в переменных
-        // firstMeetingDate и secondMeetingDate. Можно также вызывать методы generateCity(locale),
-        // generateName(locale), generatePhone(locale) для генерации и получения в тесте соответственно города,
-        // имени и номера телефона без создания пользователя в методе generateUser(String locale) в датагенераторе
         $("[placeholder='Город']").setValue(validUser.getCity());
         $("[placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.SHIFT, Keys.UP), Keys.DELETE);
         $("[placeholder='Дата встречи']").setValue(firstMeetingDate);
